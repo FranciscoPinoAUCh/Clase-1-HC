316e683d7958a7048c5d74c5f0584d56d92791e4
Francisco Pino A <francisco.pino.a@ug.uchile.cl>
2025-08-11 16:59:04 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -9,8 +9,11 @@ function draw() {
   d = random(10,60);
   fill(255,random(55,200));
   ellipse(mouseX,mouseY,d,d);
-  function keyPressed() {
+}
+
+function keyPressed() {
   if (key === 'p') {
     save('dibujo.jpg');
   }
-  }
+}
+ 
