556e52685f0d071665d0eaa48212d26aa5707212
Francisco Pino A <francisco.pino.a@ug.uchile.cl>
2025-08-18 17:57:34 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,20 +1,15 @@
-var d;
-
 function setup() {
-   createCanvas(windowWidth, windowHeight).position(0,0).style('z-index','-1');
-   background(180,40,40);
+  createCanvas(windowWidth, windowHeight).position(0,0).style('z-index','-1');
+   background(255,216,223);
 }
-
 function draw() {
-  d = random(10,60);
+  d=random(50,20);
   colorMode(HSB,360,100,100);
   fill(random(0,360),100,100);
   ellipse(mouseX,mouseY,d,d);
 }
-
 function keyPressed() {
   if (key === 'p') {
     save('dibujo.jpg');
   }
 }
- 
